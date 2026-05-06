a54f1e5f65a27ccf5721aa6ae72ea39cba9e8660
murad403 <mdm78774@gmail.com>
2026-05-06 14:17:50 +0600

    add authentication pages and layout components; implement splash screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+"use client";
+import { useEffect, useState } from "react";
+import SignIn from "./(auth)/auth/sign-in/page";
+import Splash from "@/components/shared/Splash";
 
-const page = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default page
+
+const Page = () => {
+  const [showSplash, setShowSplash] = useState(() => true);
+
+  useEffect(() => {
+    const timerId = window.setTimeout(() => {
+      setShowSplash(false);
+    }, 2000);
+
+    return () => {
+      window.clearTimeout(timerId);
+    };
+  }, []);
+
+  return showSplash ? <Splash /> : <SignIn />;
+};
+
+export default Page;
